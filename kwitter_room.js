da8635a9d48c8089e90c08c95ff65bfc74eb9e25
Ansh651 <72167893+Ansh651@users.noreply.github.com>
2021-01-01 16:01:03 -0600

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,15 +1,17 @@
 
 //ADD YOUR FIREBASE LINKS HERE
-// Your web app's Firebase configuration
-var firebaseConfig = {
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
       apiKey: "AIzaSyCKNOmCMXX-PAVQ-QdLwZO6K4_qUAvNkd0",
       authDomain: "letschat-d2203.firebaseapp.com",
+      databaseURL: "https://letschat-d2203-default-rtdb.firebaseio.com",
       projectId: "letschat-d2203",
       storageBucket: "letschat-d2203.appspot.com",
       messagingSenderId: "19587294804",
       appId: "1:19587294804:web:07e2a54ea560e237e8efc1"
     };
     // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
